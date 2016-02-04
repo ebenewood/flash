ee4d4bbdf5e889278e3a7838d36f5230441d4911
Eben <ebenewood@yahoo.com>
2016-02-03 20:18:46 -0800

    js file bascially complete

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,59 @@
 // });
 
 $(function() {
-  $("#mod").click(function() {
+  $("#jav").click(function() {
     alert("an object-oriented computer programming language commonly used to create interactive effects within web browsers.");
   });
+  $("#ope").click(function() {
+    alert(". In mathematics and sometimes in computer programming, an operator is a character that represents an action, as for example x is an arithmetic operator that represents multiplication. In computer programs, one of the most familiar sets of operators, the Boolean operators, is used to work with true/false values. Boolean operators include AND, OR, NOT (or AND NOT), and NEAR. These (and variations such as XOR) are used in logic gate s. You may have used Boolean operators in a search engine , by entering a phrase such as Galileo AND satellite (some search engines require the operator be capitalized) to find information. Among the other types of operators used in computer programming are: assignment operators , which assign a specified value to another value and relational operators , which compare two values.");
+  });
+  $("#nan").click(function() {
+    alert("In computing, NaN, standing for not a number, is a numeric data type value representing an undefined or unrepresentable value, especially in floating-point calculations. Systematic use of NaNs was introduced by the IEEE 754 floating-point standard in 1985, along with the representation of other non-finite quantities like infinities. Two separate kinds of NaNs are provided, termed quiet NaNs and signaling NaNs. Quiet NaNs are used to propagate errors resulting from invalid operations or values, whereas signaling NaNs can support advanced features such as mixing numerical and symbolic computation or other extensions to basic floating-point arithmetic. For example, 0/0 is undefined as a real number, and so represented by NaN; the square root of a negative number is imaginary, and thus not representable as a real floating-point number, and so is represented by NaN; and NaNs may be used to represent missing values in computations.");
+  });
+
+  $("#var").click(function() {
+    alert("In computer programming, a naming convention is a set of rules for choosing the character sequence to be used for identifiers which denote variables, types, functions, and other entities in source code and documentation.Reasons for using a naming convention (as opposed to allowing programmers to choose any character sequence) include the following:to reduce the effort needed to read and understand source code;to enable code reviews to focus on more important issues than arguing over syntax and naming standards.to enable code quality review tools to focus their reporting mainly on significant issues other than syntax and style preferences.to enhance source code appearance (for example, by disallowing overlong names or unclear abbreviations).The choice of naming conventions can be an enormously controversial issue, with partisans of each holding theirs to be the best and others to be inferior. Colloquially, this is said to be a matter of dogma.[2] Many companies have also established their own set of conventions to best meet their interests.");
+  });
+  $("#plusminus").click(function() {
+    alert("operator example x += y same as x = x + y");
+  });
+  $("#fun").click(function() {
+    alert("n this sense, a function is a type of procedure or routine. Some programming languages make a distinction between a function, which returns a value, and a procedure, which performs some operation but does not return a value. Most programming languages come with a prewritten set of functions that are kept in a library.");
+  });
+  $("#met").click(function() {
+    alert("A method is a set of code which is referred to by name and can be called (invoked) at any point in a program simply by utilizing the method's name. Think of a method as a subprogram that acts on data and often returns a value. Each method has its own name.");
+  });
+  // $("arg").click(function() {
+  //   alert("All arguments to methods in Java are pass-by-value.Pass-by-value means that when you call a method, a copy of each actual parameter (argument) is passed.   You can change that copy inside the method, but this will have no effect on the actual parameter. Unlike many other languages, Java has no mechanism to change the value of an actual parameter. Isn't this very restrictive?  Not really. In Java, we can pass a reference to an object (also called a "handle")as a parameter. We can then change something inside the object; we just can't change what object the handle refers to. ");
+  // });
+  // $("str").click(function() {
+  //   alert(" linear sequence of characters, words, or other data.");
+  // });
+  // $("cha").click(function() {
+  //   alert("Method chaining, also known as named parameter idiom, is a common syntax for invoking multiple method calls in object-oriented programming languages. Each method returns an object, allowing the calls to be chained together in a single statement without requiring variables to store the intermediate results.[1] Local variable declarations are syntactic sugar because of the difficulty humans have with deeply nested method calls.[2][3] A method chain is also known as a train wreck due to the increase in the number of methods that come one after another in the same line that occurs as more methods are chained together[4] even though line breaks are often added between methods.");
+  // });
+  // $("esc").click(function() {
+  //   alert("A character preceded by a backslash "\" is an escape sequence and has special meaning to the compiler. The following table shows the Java escape sequences: When an escape sequence is encountered in a print statement, the compiler interprets it accordingly. For example, if you want to put quotes within quotes you must use the escape sequence, "\", on the interior quotes. To print the sente");
+  // });
+  // $("ale").click(function() {
+  //   alert("he alert() method displays an alert box with a specified message and an OK button.");
+  // });
+  // $("com").click(function() {
+  //   alert("In computer programming, a comment is a programmer-readable annotation in the source code of a computer program. They are added with the purpose of making the source code easier to understand, and are generally ignored by compilers and interpreters.[1][2] The syntax of comments in various programming languages varies considerably.");
+  // });
+  // $("boo").click(function() {
+  //   alert("In computer science, the Boolean data type is a data type, having two values (usually denoted true and false), intended to represent the truth values of logic and Boolean algebra. It is named after George Boole, who first defined an algebraic system of logic in the mid 19th century.");
+  // });
+  // $("und").click(function() {
+  //   alert("An undefined variable in the source code of a computer program is a variable that is accessed in the code but has not been previously declared by that code.[1]");
+  // });
+  // // $("par").click(function() {
+  // //   alert("");
+  // // });
+  // // $("ret").click(function() {
+  // //   alert("");
+  // // });
+  // $("mod").click(function() {
+  //   alert("With respect to or using a modulus of a specified number. Two numbers are congruent modulo a given number if they give the same remainder when divided by that number.");
+  // });
 });
